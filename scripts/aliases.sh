5dd5d67a150ac286d1d9d8ac3eb3ebaed17f766c
alishahid74 <93749056+alishahid74@users.noreply.github.com>
2025-10-04 14:06:52 -0400

    Delete scripts directory

--- scripts/aliases.sh
+++
@@ -1,7 +0,0 @@
-alias avbin='~/lab/evasive-angr/samples/antidebug_demo'
-
-angr_cfg_inj () { python3 scripts/cfg_injection_finder.py "${1:-$(avbin)}" | tee "adv_results/cfg_injection_$(basename ${1:-$(avbin)}).log"; }
-
-angr_adv_det () { python3 scripts/advanced_angr_detector.py "${1:-$(avbin)}" 2>&1 | tee "adv_results/advanced_detector_$(basename ${1:-$(avbin)}).log"; }
-
-angr_imports () { python3 scripts/list_imports.py "${1:-$(avbin)}" | tee "adv_results/imports_$(basename ${1:-$(avbin)}).log"; }
